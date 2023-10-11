f54bed63121401e9b76836b3070a55321dfea4bc
runqingc <cairunqing02@outlook.com>
2023-10-11 08:24:31 -0500

    initialize project, load maven dependencies

---
+++ src/main/java/runqingc/money_splittor/MoneySplittorApplication.java
@@ -0,0 +1,13 @@
+package runqingc.money_splittor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneySplittorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneySplittorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/runqingc/money_splittor/MoneySplittorApplicationTests.java
@@ -0,0 +1,13 @@
+package runqingc.money_splittor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneySplittorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
