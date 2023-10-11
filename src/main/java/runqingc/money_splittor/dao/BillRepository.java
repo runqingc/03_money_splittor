23ee799346655c7b9d2d3e57d48a46055a4779bc
runqingc <cairunqing02@outlook.com>
2023-10-11 09:29:43 -0500

    updated BillRepository

---
+++ src/main/java/runqingc/money_splittor/dao/BillRepository.java
@@ -0,0 +1,10 @@
+package runqingc.money_splittor.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import runqingc.money_splittor.entity.Bill;
+
+public interface BillRepository extends JpaRepository<Bill, Integer> {
+
+    // got all the code for free!
+
+}
